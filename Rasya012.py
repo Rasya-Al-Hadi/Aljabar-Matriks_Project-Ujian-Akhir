20f53d3ad09455d7f28bf252958c6bb3b04b38d1
Rasya-Al-Hadi <rasya.dzakira2007@gmail.com>
2026-06-13 22:26:04 +0700

    Add files via upload

--- Rasya012.py
+++ Rasya012.py
@@ -25,14 +25,25 @@ def kali_matriks(A, B):
     for i in range(3):
         for j in range(3):
             for k in range(3):
-                hasil[i][j] += A[i][k] * B[k][j]
+                hasil[i][j] = hasil[i][j] + (A[i][k] * B[k][j])
     return hasil
 
+def determinan_sarrus(matriks):
+    a, b, c = matriks[0]
+    d, e, f = matriks[1]
+    g, h, i = matriks[2]
 
-def determinan_3x3(M):
-    det = (
-        M[0][0] * (M[1][1] * M[2][2] - M[1][2] * M[2][1])
-        - M[0][1] * (M[1][0] * M[2][2] - M[1][2] * M[2][0])
-        + M[0][2] * (M[1][0] * M[2][1] - M[1][1] * M[2][0])
-    )
-    return det
+    d1 = a * e * i
+    d2 = b * f * g
+    d3 = c * d * h
+
+    s1 = c * e * g
+    s2 = b * d * i
+    s3 = a * f * h
+
+    determinan = (d1 + d2 + d3) - (s1 + s2 + s3)
+
+    print("Diagonal utama  :", d1, "+", d2, "+", d3, "=", d1 + d2 + d3)
+    print("Diagonal sekunder:", s1, "+", s2, "+", s3, "=", s1 + s2 + s3)
+
+    return determinan
